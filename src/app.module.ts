21d109b29ad65dca9031e9c323924a6e5725ffc4
서지유 <jihoseo2008@gmail.com>
2026-06-09 20:24:51 +0900

    feat :: setting server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { RoadmapsModule } from './roadmaps/roadmaps.module';
+import { NotesModule } from './notes/notes.module';
+import { CommunityModule } from './community/community.module';
+import { AiModule } from './ai/ai.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    RoadmapsModule,
+    NotesModule,
+    CommunityModule,
+    AiModule,
+  ],
 })
 export class AppModule {}
