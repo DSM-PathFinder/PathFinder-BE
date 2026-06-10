26e0d501f30832fe9ce20aa6a241c06e10aa6b27
서지유 <jihoseo2008@gmail.com>
2026-06-10 09:22:25 +0900

    fix :: maria db adapter

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,20 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
+    console.log('✅ Prisma connected to MySQL');
   }
 
   async onModuleDestroy() {
